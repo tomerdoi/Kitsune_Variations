7b9481058a20b666e827c87151b06004b6165a3f
tomerdoi <tomerdoi@post.bgu.ac.il>
2017-12-11 14:30:02 +0200

    Creation

--- FuzzyClusterer.py
+++ FuzzyClusterer.py
@@ -30,7 +30,9 @@ def clusterUsingIncrementalFuzzyDendogram(data, maxSizeOfCluster=10, maxFuz=3):
     data=np.transpose(data)
     clusters=[]
     centroids=[]
-    for featureRow in data:
+    featuresIndexes=[]
+    for j,featureRow in enumerate(data):
+        featuresIndexes.append([j])
         clusters.append([featureRow])
         centroids.append(np.average(featureRow))
 
@@ -57,7 +59,7 @@ def clusterUsingIncrementalFuzzyDendogram(data, maxSizeOfCluster=10, maxFuz=3):
 
         for c2 in range(len(centroids)):
             if c2!=minC1:
-                c1c2Diff = np.abs(centroids[c1] - centroids[c2])
+                c1c2Diff = np.abs(centroids[minC1] - centroids[c2])
                 distAr.append(c1c2Diff)
                 indexesAr.append(c2)
 
@@ -67,19 +69,21 @@ def clusterUsingIncrementalFuzzyDendogram(data, maxSizeOfCluster=10, maxFuz=3):
         for idx in range(maxFuz):
 
             if idx<maxFuz-1:
-                mergeClusters(clusters,centroids,minC1,indexesAr[idx],False)
+                mergeClusters(clusters,featuresIndexes,centroids,minC1,indexesAr[idx],False)
             else:
-                mergeClusters(clusters, centroids, minC1, indexesAr[idx], True)
+                mergeClusters(clusters,featuresIndexes, centroids, minC1, indexesAr[idx], True)
 
 
         actualSizeOfCluster = max([len(clusters[i]) for i in range(len(clusters))])
     print('finished clustering...')
 
 
-def mergeClusters(clusters,centroids,c1,c2, isLast=False):
+def mergeClusters(clusters,featuresIndexes,centroids,c1,c2, isLast=False):
     clusters[c1].extend(clusters[c2])
+    featuresIndexes[c1].extend(featuresIndexes[c2])
     centroids[c1]=np.average(clusters[c1])
 
     if isLast==True:
         del clusters[c2]
         del centroids[c2]
+        del featuresIndexes[c2]
