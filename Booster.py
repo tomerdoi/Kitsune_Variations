72bdf2b68b795674e993b19c91594e2db817327d
tomerdoi <tomerdoi@post.bgu.ac.il>
2017-12-28 14:23:32 +0200

    boost

--- Booster.py
+++ Booster.py
@@ -1,6 +1,6 @@
 import os
 import time
-
+import random as rand
 import numpy as np
 import pandas as pd
 
@@ -15,12 +15,26 @@ class Booster(object):
     def __init__ (self):
         print('Booster initiated...')
 
-    def setWeightsToSamples (self, samplesRMSE,samplesIndexes):
+    def setWeightsToSamples (self, samplesRMSE):
+        samplesIndexes=[i for i in range(len(samplesRMSE))]
         samplesIndexes= [x for _,x in sorted(zip(samplesRMSE,samplesIndexes))]
         samplesRMSE=sorted(samplesRMSE)
 
         samplesWeights=np.zeros(len(samplesRMSE))
-        maxRMSE,minRMSE=max(samplesRMSE),min(samplesRMSE)
+        maxRMSE,minRMSE=max(samplesRMSE)+2,min(samplesRMSE)+5
         for idx in range(len(samplesRMSE)):
-            samplesWeights[samplesIndexes[idx]]=(samplesRMSE[samplesIndexes[idx]]-minRMSE)/(maxRMSE-minRMSE+1**-6)
-            
+            samplesWeights[samplesIndexes[idx]]=(samplesRMSE[samplesIndexes[idx]]-minRMSE)/(maxRMSE-minRMSE+1**-6) #to normalize again
+
+        self.samplesWeights=samplesWeights
+        self.samplesIndexes=samplesIndexes
+
+    def randomBasedOnWeights (self,samples):
+        weightsSum=sum(self.samplesWeights)
+        randSample=rand.randrange(0,weightsSum)
+
+        newSum=0
+        for idx in range(len(self.samplesWeights)):
+            newSum+=self.samplesWeights[idx]
+            if (newSum>=randSample):
+                return idx
+
