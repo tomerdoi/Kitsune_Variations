af640fad47de1c43d00573ca8ae7c1a3a222b330
tomerdoi <tomerdoi@post.bgu.ac.il>
2018-05-23 16:37:09 +0300

    c1

--- Datasets/DatasetGenerators/seqXtractor/seqFE.py
+++ Datasets/DatasetGenerators/seqXtractor/seqFE.py
@@ -1,5 +1,5 @@
-
-
+import ML_models.HMM as hmm
+import numpy as np
 
 #mac-mac,ip, ip-ip, ip_port-ip_port
 # windows: 10ms,100ms,1s,5s,10s
@@ -15,3 +15,95 @@
 # HMM of dstIP: transfers of srcIP in the last 20 packets (20probs)
 # HMM of srcMac: transfers of srcMac in the last 20 packets (20probs)
 # HMM of dstMac: transfers of dstMac in the last 20 packets (20probs)
+
+
+class seqFE ():
+
+
+    def __init__ (self):
+
+        self.windows=[0.01,0.1,1,5,10] # in seconds
+        self.entities=[] #all the hosts in the network
+        self.host2LastTS = {}
+
+        self.host2WindowTSDelay={}
+        self.host2WindowBR={}
+        self.host2WindowFramelen={}
+
+        self.hmmProt=hmm.HMM(0)
+        self.hmmSrcIP=hmm.HMM(0)
+        self.hmmDstIP=hmm.HMM(0)
+        self.hmmSrcMac=hmm.HMM(0)
+        self.hmmDstMac=hmm.HMM(0)
+
+
+
+
+
+    def update (self,srcIP,srcMAC,srcPort,dstIP,dstMAC,dstPort,framelen,ts):
+
+        entities=[srcIP,srcMAC+'-'+dstMAC,srcIP+'-'+dstIP,srcIP+'_'+srcPort+'-'+dstIP+'_'+dstPort]
+
+        for e in entities:
+
+            #new entity
+            if e not in self.entities:
+                self.entities.append(e)
+                self.host2LastTS[e]=ts
+
+                for w in self.windows:
+                    self.host2WindowTSDelay[e + '_' + str(w)]=[]
+                    self.host2WindowBR[e + '_' + str(w)]=[]
+                    self.host2WindowFramelen[e + '_' + str(w)]=[]
+
+                lastProt=dstPort
+                lastSrcIp=srcIP
+                lastDstIp=dstIP
+                lastSrcMac=srcMAC
+                lastDstMac=dstMAC
+
+
+
+            # updating entity Delay&BR&FrameLen
+            for w in self.windows:
+
+                self.host2WindowTSDelay[e+'_'+str(w)].append(float(ts-self.host2LastTS[e]))
+                self.host2WindowBR[e + '_' + str(w)].append(framelen)
+                self.host2WindowFramelen[e + '_' + str(w)].append(framelen)
+
+                while float(ts-self.host2WindowTSDelay[e+'_'+str(w)][0])>w and len(self.host2WindowTSDelay[e+'_'+str(w)])>1:
+                    del self.host2WindowTSDelay[e+'_'+str(w)][0]
+                    del self.host2WindowBR[e+'_'+str(w)][0]
+                    del self.host2WindowFramelen[e+'_'+str(w)][0]
+
+            self.host2LastTS[e] = ts
+
+            # updating entity HMMs
+
+        self.hmmProt.getNewState(dstPort)
+        self.hmmSrcIP.getNewState(srcIP)
+        self.hmmDstIP.getNewState(dstIP)
+        self.hmmSrcMac.getNewState(srcMAC)
+        self.hmmDstMac.getNewState(dstMAC)
+
+        lastProt = dstPort
+        lastSrcIp = srcIP
+        lastDstIp = dstIP
+        lastSrcMac = srcMAC
+        lastDstMac = dstMAC
+
+
+
+s=seqFE()
+s.update('1.1.1.1','AA:AA:AA:AA:AA:AA','5000','2.2.2.2','BB:BB:BB:BB:BB:BB','80',1260,1.1)
+s.update('2.2.2.2','BB:BB:BB:BB:BB:BB','6000','6.2.2.2','FF:FF:FF:FF:FF:FF','90',1270,1.2)
+s.update('3.3.3.3','CC:CC:CC:CC:CC:CC','7000','7.2.2.2','GG:GG:GG:GG:GG:GG','100',1280,1.3)
+s.update('4.4.4.4','DD:DD:DD:DD:DD:DD','8000','8.2.2.2','HH:HH:HH:HH:HH:HH','110',1290,1.4)
+s.update('5.5.5.5','EE:EE:EE:EE:EE:EE','9000','9.2.2.2','II:II:II:II:II:II','120',12100,1.5)
+
+s.update('3.3.3.3','CC:CC:CC:CC:CC:CC','7000','7.2.2.2','GG:GG:GG:GG:GG:GG','100',1280,1.8)
+print('finished')
+
+
+
+
--- ML_models/HMM.py
+++ ML_models/HMM.py
@@ -6,11 +6,36 @@ class HMM (object):
     def __init__ (self,NumOfStates):
         self.numOfStates=NumOfStates
         self.m_transitionMatrix=np.zeros((NumOfStates,NumOfStates))
-        self.currState=1
+        self.currState=-1
+        self.nomimalToIndex={}
+
+    def copyAndAddOneToMats(self):
+
+        copy = np.zeros((self.m_transitionMatrix.shape[0] + 1, self.m_transitionMatrix.shape[1] + 1))
+        for i in range(self.m_transitionMatrix.shape[0]):
+            for j in range(self.m_transitionMatrix.shape[1]):
+                copy[i][j] = self.m_transitionMatrix[i][j]
+
+        self.m_transitionMatrix=copy
+        self.numOfStates+=1
 
     def getNewState(self,state):
-        self.m_transitionMatrix[self.currState-1][state-1]+=1
+
+        if state not in self.nomimalToIndex:
+
+            self.copyAndAddOneToMats()
+
+            self.nomimalToIndex[state]=self.numOfStates-1
+
+        state=self.nomimalToIndex[state]
+
+        if self.currState==-1:
+            self.currState=0
+            return
+
+        self.m_transitionMatrix[self.currState][state]+=1
         self.currState=state
+
 
     def setState (self,state):
         self.currState=state
@@ -21,17 +46,17 @@ class HMM (object):
             ans=np.matmul(m1,ans)
         return ans
 
-h=HMM(5)
-numberOfIters=random.randint(1000000,1000000)
-for i in range(numberOfIters):
-    newState=random.randint(1,h.numOfStates)
-    h.getNewState(newState)
-
-for r in range(len(h.m_transitionMatrix)):
-    s=sum(h.m_transitionMatrix[r])
-    for j in range(len(h.m_transitionMatrix[r])):
-        h.m_transitionMatrix[r][j]/=float(s)
-
-
-print(h.m_transitionMatrix)
-
+# h=HMM(5)
+# numberOfIters=random.randint(1000000,1000000)
+# for i in range(numberOfIters):
+#     newState=random.randint(1,h.numOfStates)
+#     h.getNewState(newState)
+#
+# for r in range(len(h.m_transitionMatrix)):
+#     s=sum(h.m_transitionMatrix[r])
+#     for j in range(len(h.m_transitionMatrix[r])):
+#         h.m_transitionMatrix[r][j]/=float(s)
+#
+#
+# print(h.m_transitionMatrix)
+#
